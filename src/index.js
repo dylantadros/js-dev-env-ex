ba2f9b5a4b1caaf1df959861242fcd1bfc4319eb
Dylan Tadros <dylan.tadros@gmail.com>
2017-02-01 10:33:08 -0500

    linting sipport

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-import numeral from 'numeral';
+//import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`You couldnt pay me ${courseValue} to take this course!`);
+//const courseValue = numeral(1000).format('$0,0.00');
+//console.log(`You couldnt pay me ${courseValue} to take this course!`);
